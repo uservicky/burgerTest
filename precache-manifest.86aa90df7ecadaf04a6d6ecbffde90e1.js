f05b67cd761600fd27a3bd59cdbc5f532c85bb4e
vicky sharma <uservickysharma@gmail.com>
2020-02-23 23:46:50 +0530

    Updates

---
+++ precache-manifest.86aa90df7ecadaf04a6d6ecbffde90e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f26836c95f7253a01131fde3cec4226",
+    "url": "/burgerTest/index.html"
+  },
+  {
+    "revision": "13e13e4ed4144d9cde83",
+    "url": "/burgerTest/static/css/main.149662cc.chunk.css"
+  },
+  {
+    "revision": "bcd8d2352b4eabd32e3d",
+    "url": "/burgerTest/static/js/2.fe381669.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/burgerTest/static/js/2.fe381669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13e13e4ed4144d9cde83",
+    "url": "/burgerTest/static/js/main.317c54d8.chunk.js"
+  },
+  {
+    "revision": "c0359edfaf55ee133918",
+    "url": "/burgerTest/static/js/runtime-main.99c6ac84.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerTest/static/media/burger-logo.b8503d26.png"
+  }
+]);
